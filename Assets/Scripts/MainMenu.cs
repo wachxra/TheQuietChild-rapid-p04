507edd711e205c641f24e657431beff4feadd360
wachxra <wachirawadee.junt@bumail.net>
2025-11-25 06:19:03 +0700

    Added Basic Main Menu scene (Start and exit)

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame(string sceneName)
+    {
+        Debug.Log("Loading scene: " + sceneName);
+
+        if (!string.IsNullOrEmpty(sceneName))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
