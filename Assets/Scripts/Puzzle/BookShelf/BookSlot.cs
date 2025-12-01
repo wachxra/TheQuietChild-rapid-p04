c182e2413a9da48921a15e4312f3845ef448c1c6
wachxra <wachirawadee.junt@bumail.net>
2025-12-01 18:00:57 +0700

    Fixed: BookSlot frame

--- Assets/Scripts/Puzzle/BookShelf/BookSlot.cs
+++ Assets/Scripts/Puzzle/BookShelf/BookSlot.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class BookSlot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
@@ -19,18 +20,6 @@ public class BookSlot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
         canvas = GetComponentInParent<Canvas>();
     }
 
-    void Update()
-    {
-        if (placeholder != null)
-        {
-            placeholder.transform.localPosition = Vector3.Lerp(
-                placeholder.transform.localPosition,
-                parentBeforeDrag.GetChild(placeholder.transform.GetSiblingIndex()).localPosition,
-                Time.deltaTime * 15f
-            );
-        }
-    }
-
     public void OnBeginDrag(PointerEventData eventData)
     {
         if (!canDrag) return;
@@ -38,12 +27,20 @@ public class BookSlot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
         parentBeforeDrag = transform.parent;
 
         placeholder = new GameObject("Placeholder");
+        placeholder.name = "Placeholder for " + gameObject.name;
+
         var rt = placeholder.AddComponent<RectTransform>();
         rt.sizeDelta = rectTransform.sizeDelta;
+
+        placeholder.AddComponent<LayoutElement>();
+        placeholder.AddComponent<CanvasGroup>().blocksRaycasts = false;
+
         placeholder.transform.SetParent(parentBeforeDrag);
         placeholder.transform.SetSiblingIndex(transform.GetSiblingIndex());
 
         transform.SetParent(canvas.transform);
+
+        LayoutRebuilder.ForceRebuildLayoutImmediate(parentBeforeDrag as RectTransform);
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -53,8 +50,8 @@ public class BookSlot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
         rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
 
         float draggedX = rectTransform.position.x;
-
         int newIndex = parentBeforeDrag.childCount - 1;
+        bool indexChanged = false;
 
         for (int i = 0; i < parentBeforeDrag.childCount; i++)
         {
@@ -70,7 +67,16 @@ public class BookSlot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
             }
         }
 
-        placeholder.transform.SetSiblingIndex(newIndex);
+        if (placeholder.transform.GetSiblingIndex() != newIndex)
+        {
+            placeholder.transform.SetSiblingIndex(newIndex);
+            indexChanged = true;
+        }
+
+        if (indexChanged)
+        {
+            LayoutRebuilder.ForceRebuildLayoutImmediate(parentBeforeDrag as RectTransform);
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
@@ -78,14 +84,12 @@ public class BookSlot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
         if (!canDrag || manager == null) return;
 
         int targetIndex = placeholder.transform.GetSiblingIndex();
-
         transform.SetParent(parentBeforeDrag);
         transform.SetSiblingIndex(targetIndex);
-
         manager.SwapBooks(this, targetIndex);
 
         Destroy(placeholder);
-
         rectTransform.anchoredPosition = Vector2.zero;
+        LayoutRebuilder.ForceRebuildLayoutImmediate(parentBeforeDrag as RectTransform);
     }
 }
